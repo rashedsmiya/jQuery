ae89c978522926f8baa1d9032435b40789da5d7c
rashedsmiya <rashedmiyaofficial@gmail.com>
2023-07-22 13:23:45 +0600

    ptag-added

---
+++ jQurey- P - 05/js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    
+//    $('h2').click(function(){
+//         // alert("h2 clicked");
+//         $('a').hide();
+
+$('h2').click(function(){
+//    $('.one h2').hide();
+      $(this).hide();
+});
+
+});
+
+ 
