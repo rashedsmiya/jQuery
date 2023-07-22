da4db4f61b4afbaf4f29286b7fdd25684991fb4c
rashedsmiya <rashedmiyaofficial@gmail.com>
2023-07-22 11:05:27 +0600

    ptag-added

---
+++ jQurey- P - 04/js/app.js
@@ -0,0 +1,18 @@
+// $('.one').hide();
+
+// jQuery('jar oper kaj korte chaicho take select koro'). Jake tumi select korecho Tar opor Tumi ki kaj korte chao()
+
+
+// $('div').hide();
+
+
+$(document).ready(function(){
+    
+    $('.one').hide();
+
+});
+
+$(function(){
+
+
+});
