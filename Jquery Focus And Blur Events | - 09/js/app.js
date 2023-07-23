2c302ffd08b60c1f8d8574f6748ae1d8494743be
rashedsmiya <rashedmiyaofficial@gmail.com>
2023-07-23 19:14:31 +0600

    ptag-added

--- Jquery Focus And Blur Events | - 09/js/app.js
+++ Jquery Focus And Blur Events | - 09/js/app.js
@@ -1,46 +1,44 @@
-$(document).ready(function(){
-    // $(".one").mouseenter(function(){
-    //     alert('Hello');    
+// $(document).ready(function(){
+   // $('button').click();
 
-    // });
+   // $('button').dblclick();
 
-    // $(".one").mousedown(function(){
-    //     alert('Hello');    
+   // $('button').mouseenter();
 
-    // });
+   // $('button').on({
+   //     click: function(){
+   //         console.log("click");
+   //     },
+   //     mouseenter: function(){
+   //       console.log("Double click");
+   //     }
+   // });
 
-    // $(".one").mousleve(function(){
-    //     alert('byy');    
+   // $(document).ready(function(){
 
-    // });
+   // $('button').click(function(){
+   //       $('.one > h2').hide();
+   // });
 
-    // $(".one").mouseup(function(){
-    //     alert('byy');    
+   // $('.two button').click(function(){
+   //       $('.two > h2').hide();
+   // });
 
-    // });
+   // $('.two button').on('click', function(){
+   //       $('.two > h2').hide();
+   // });
 
-    // $(".one").mousedown(function(){
-    //     alert('Hello');    
+   $(document).ready(function(){
+      
+      $('input').focus(function(){
+         $(this).val('Focused');
+      });
 
-    // });
+      $('input').blur(function(){
+         $(this).val('Blur');
+      });
 
-        //    $('.one').mouseenter(function(){
-        //       alert('Buy');
-        //    });
-        //    $('.one').mouseleave(function(){
-        //     alert('Hello');
-        //  });
-
-         $('.one').hover(function(){
-            console.log("Enter");
-         }, function(){
-            console.log("Leave");
-         });
-         
-    });
-
-
-
+   });
 
 
 
---
+++ Jquery Hide And Show  | P - 10/js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  
+    // $('.hide_me').click(function(){
+     
+    //   $('.box p').hide(2000);
+
+    // });
+
+    // $('.show-me').click(function() {
+    //    $('.box p').show(2000);
+    // });
+
+    $('button').click(function(){
+      
+      $('.box p').toggle(3000);
+
+    });
+
+});
