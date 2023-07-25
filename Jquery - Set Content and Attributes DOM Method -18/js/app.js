d9b807692dd47ce46152348042812b7b01bd2ad0
rashedsmiya <rashedmiyaofficial@gmail.com>
2023-07-25 19:33:39 +0600

    ptag-added

--- Jquery - Set Content and Attributes DOM Method -18/js/app.js
+++ Jquery - Set Content and Attributes DOM Method -18/js/app.js
@@ -1,4 +1,47 @@
 $(document).ready(function(){
-    let oneVal = $('.one').text();
-    $('.two').text("Raj");
-});
+
+    //  $('button').click(function(){
+        // let oneVal = $('.one').text();
+        // $('.two').text(oneVal);
+
+        // let htmlVal = '<strong>Hii</strong>';
+        // $('.two').html(htmlVal);
+
+        // let htmlVal =  '<div class="three"> \
+        //                <div class="four">Four</div> \
+        //                </div>';
+        //                $('.two').html(htmlVal);
+
+
+        // $('#fname').val("hello");
+
+        // $('button').click(function(){
+        //     $('.one a').attr('href', 'http://www.google.com/')
+        //                .attr('target', '_blank');
+
+        //                $('.one a').attr({
+        //                 'href': 'http://www.google.com/',
+        //                 'target': '_blank'
+        //                });
+
+
+
+        // });
+
+        $('button').click(function(){
+              $('.one').text("Hii");
+              $('.one').attr('title', function(index, text){
+                  return text + " New Text";
+              });
+
+              
+
+
+           });
+
+        });
+
+
+
+
+    
